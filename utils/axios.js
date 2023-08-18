c6eda180b21766bb5efe4958d18ca4d67eb6b38f
master-dhanesh <dhanesh1296@gmail.com>
2023-08-19 04:12:12 +0530

    final commit

--- utils/axios.js
+++ utils/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8080/",
-  withCredentials: true,
+    baseURL: "https://backend-d9c4.onrender.com" || "http://localhost:8080/",
+    withCredentials: true,
 });
 
 export default instance;
